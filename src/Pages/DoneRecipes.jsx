401ab45b67e06acac550d7d462c8648319a609d1
Jessica Naiara <jessicanaiarag@gmail.com>
2021-10-05 16:54:25 -0300

    requisitos 5

--- src/Pages/DoneRecipes.jsx
+++ src/Pages/DoneRecipes.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import HeaderWithoutSearch from './HeaderWithoutSearch';
 
 export default function DoneRecipes() {
+  const lsDoneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+
   return (
     <>
       <HeaderWithoutSearch />
       <h3 data-testid="page-title">Receitas Feitas</h3>
-      {/*
       <Link to=" ">
         <button type="button" data-testid="filter-by-all-btn">
           All
@@ -23,16 +24,28 @@ export default function DoneRecipes() {
           Drinks
         </button>
       </Link>
-      <div>
-        <img
-          src={ `${index}` }
-          alt="img-Details"
-          data-testid={ `${index}-horizontal-image` }
-        />
-        <p data-testid={ `${index}-horizontal-top-text` } />
-        <p data-testid={ `${index}-horizontal-name` } />
-        <p data-testid={ `${index}-horizontal-done-date` } />
-      </div> */}
+      {lsDoneRecipes.map((recipe, index) => (
+        <div key={ index }>
+          {recipe.tags.map((tag, index1) => (
+            <p
+              key={ index1 }
+              data-testid={ `${index}-${recipe.tags[index1]}-horizontal-tag` }
+            >
+              {tag}
+            </p>))}
+          <img
+            src={ `${index}` }
+            alt="img-Details"
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <p data-testid={ `${index}-horizontal-top-text` } />
+          <p data-testid={ `${index}-horizontal-name` } />
+          <p data-testid={ `${index}-horizontal-done-date` } />
+          <button type="button" data-testid={ `${index}-horizontal-share-btn` }>
+            Compartilhar
+          </button>
+        </div>))}
+
     </>
   );
 }
