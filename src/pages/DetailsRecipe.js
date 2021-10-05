2c5c978ce1eff190bf62cefa34620b367f5b8e4d
alexcorreadasilva27 <alexcorreas1994@gmail.com>
2021-10-05 20:45:01 -0300

    Comentando os avisos de lint

--- src/pages/DetailsRecipe.js
+++ src/pages/DetailsRecipe.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState, useCallback } from 'react';
 import { useLocation, useParams, useHistory } from 'react-router-dom';
 import ReactPlayer from 'react-player';
