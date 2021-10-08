6039c4cf3a88ac6e81953cb225a5f5d8435baed5
EddieRAM <edusacarneiro@gmail.com>
2021-10-07 21:27:32 -0300

    appetizing

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Form } from 'react-bootstrap';
+import '../Styles/Login.css';
 
 export default function Login({ history }) {
   const [email, setEmail] = useState('');
@@ -23,37 +24,44 @@ export default function Login({ history }) {
   }
 
   return (
-    <Form className="login-form">
-      <h1>Login</h1>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control
-          type="email"
-          data-testid="email-input"
-          onChange={ ({ target }) => setEmail(target.value) }
-          placeholder="Insira seu e-mail"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Control
-          type="password"
-          data-testid="password-input"
-          onChange={ ({ target }) => setPassword(target.value) }
-          placeholder="Insira sua senha"
-        />
-      </Form.Group>
-      <Button
-        className="login-button"
-        type="button"
-        size="lg"
-        variant="success"
-        data-testid="login-submit-btn"
-        disabled={ buttonStatus }
-        onClick={ handleClick }
-      >
-        Entrar
-      </Button>
-    </Form>
-
+    <div className="login">
+      <div className="login-container">
+        <Form className="login-form">
+          <h1 className="login-title">Login</h1>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              className="login-input"
+              type="email"
+              data-testid="email-input"
+              onChange={ ({ target }) => setEmail(target.value) }
+              placeholder="Insira seu e-mail"
+            />
+            <span className="login-input-border" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+              className="login-input"
+              type="password"
+              data-testid="password-input"
+              onChange={ ({ target }) => setPassword(target.value) }
+              placeholder="Insira sua senha"
+            />
+            <span className="login-input-border" />
+          </Form.Group>
+          <Button
+            className="login-submit"
+            type="button"
+            size="lg"
+            variant="success"
+            data-testid="login-submit-btn"
+            disabled={ buttonStatus }
+            onClick={ handleClick }
+          >
+            Entrar
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 }
 
